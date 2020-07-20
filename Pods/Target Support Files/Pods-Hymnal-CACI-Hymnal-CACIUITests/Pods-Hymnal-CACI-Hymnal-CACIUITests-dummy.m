476f111bb293fc9b807c241817ea15a5aaabd3f9
Sandra Konotey <buelersandra@gmail.com>
2020-07-20 12:40:53 +0000

    ignore pods

--- Pods/Target Support Files/Pods-Hymnal-CACI-Hymnal-CACIUITests/Pods-Hymnal-CACI-Hymnal-CACIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hymnal_CACI_Hymnal_CACIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Hymnal_CACI_Hymnal_CACIUITests
-@end
--- Pods/Target Support Files/Pods-Hymnal-CACITests/Pods-Hymnal-CACITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hymnal_CACITests : NSObject
-@end
-@implementation PodsDummy_Pods_Hymnal_CACITests
-@end
